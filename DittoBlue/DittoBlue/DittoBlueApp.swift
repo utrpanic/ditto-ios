0a1283a315676f558fa21463c6c802b586480b28
beck <beck22j@gmail.com>
2024-03-27 23:22:22 -0400

    Initial projects of original and blue.

---
+++ DittoBlue/DittoBlue/DittoBlueApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct DittoBlueApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ditto
-It can be whatever it wants.
+# ditto-ios / 메타몽 프로젝트
+
+- It can be whatever it wants.
+- Xcode 15.3
+
